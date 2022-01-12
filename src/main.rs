a731736b601d6540bcbd9ed5b4d0dbcb6a8d5ef8
Andrew <PositiveWay05@gmail.com>
2022-01-12 16:06:00 +0400

    staged

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use gilrs::{Gilrs, Button, Event};
+use gilrs::{Gilrs, Button, Event, EventType,Axis};
+use inputbot::{KeybdKey, MouseButton, MouseWheel, MouseCursor};
+use std::{thread::sleep, time::Duration};
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate partial_application;
 
 fn main() {
     let mut gilrs = Gilrs::new().unwrap();
@@ -8,19 +14,57 @@ fn main() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    let mut active_gamepad = None;
+    let mouse_speed = 5;
+    let scroll_speed = 3;
+
+    let super_key = (0xffeb);
+    let copy_key = (KeybdKey::LControlKey, KeybdKey::CKey);
+    let paste_key = (KeybdKey::LControlKey, KeybdKey::VKey);
+
+    let commands_map = HashMap::from([
+        (Button::DPadDown, (KeybdKey::DownKey)),
+        (Button::DPadUp, (KeybdKey::UpKey)),
+        (Button::DPadLeft, (KeybdKey::LeftKey)),
+        (Button::DPadRight, (KeybdKey::RightKey)),
+        (Button::RightTrigger2, (MouseButton::LeftButton)),
+        (Button::LeftTrigger2, (MouseButton::RightButton)),
+        (Button::RightTrigger, paste_key),
+        (Button::LeftTrigger, copy_key),
+        (Button::West, (KeybdKey::EnterKey)),
+        (Button::North, (KeybdKey::SpaceKey)),
+        (Button::South, (KeybdKey::BackspaceKey)),
+        (Button::East, super_key),
+        (Axis::LeftStickX, partial!(MouseCursor::move_rel => _, 0)),
+        (Axis::LeftStickY, partial!(MouseCursor::move_rel => 0, _)),
+        (Axis::RightStickY, (MouseWheel::scroll_ver)),
+        (Axis::RightStickX, (MouseWheel::scroll_hor)),
+    ]);
+
+    let typing_map = commands_map.clone();
+
+    let mut commands_mode = true;
 
     loop {
         // Examine new events
         while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
-        }
+            println!("Event {:?} from device id {} at {:?}", event, id, time);
 
-        // You can also use cached gamepad state
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
+            let mapping = match commands_mode {
+                true => commands_map,
+                false => typing_map,
+            };
+
+            match event {
+                EventType::ButtonPressed(button,code) => {
+                    mapping[button].press();
+                },
+                EventType::ButtonReleased(button, code) => {
+                    mapping[button].release();
+                },
+                EventType::AxisChanged(axis, value, code) => {
+                    let mouse_func = mapping[axis];
+                }
+                _ => println!("Error"),
             }
         }
     }
