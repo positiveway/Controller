8cd036e85b4ed63f7313b16fcef660dc6f1fb086
Andrew <PositiveWay05@gmail.com>
2022-01-16 03:09:59 +0400

    static maps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #![feature(box_syntax)]
+
+mod uinput_direct;
+
 #[macro_use]
 extern crate partial_application;
 
@@ -6,10 +9,48 @@ use gilrs::{Gilrs, Button, Event, EventType::*, Axis};
 use inputbot::{KeybdKey, MouseButton, GenericButton, MouseWheel, MouseCursor};
 use std::{thread::sleep, time::Duration};
 use std::collections::{HashMap};
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use cached::proc_macro::cached;
 
-type BoxedButton = Box<dyn GenericButton>;
-type ButtonsVec = Vec<BoxedButton>;
+type ArcButton = Arc<dyn GenericButton + Send + Sync + 'static>;
+type ButtonsVec = Vec<ArcButton>;
 type ButtonsMapPair = (Button, ButtonsVec);
+type ButtonsMap = HashMap<Button, ButtonsVec>;
+
+lazy_static! {
+    static ref super_key: ButtonsVec = vec![Arc::new(KeybdKey::OtherKey(0xffeb))];
+    static ref copy_key: ButtonsVec = vec![Arc::new(KeybdKey::LControlKey), Arc::new(KeybdKey::CKey)];
+    static ref paste_key: ButtonsVec = vec![Arc::new(KeybdKey::LControlKey), Arc::new(KeybdKey::VKey)];
+
+    static ref CommandsMap: ButtonsMap = {
+        let _commands_map: Vec<ButtonsMapPair> = vec![
+            (Button::DPadDown, vec![Arc::new(KeybdKey::DownKey)]),
+            (Button::DPadUp, vec![Arc::new(KeybdKey::UpKey)]),
+            (Button::DPadLeft, vec![Arc::new(KeybdKey::LeftKey)]),
+            (Button::DPadRight, vec![Arc::new(KeybdKey::RightKey)]),
+            (Button::RightTrigger2, vec![Arc::new(MouseButton::LeftButton)]),
+            (Button::LeftTrigger2, vec![Arc::new(MouseButton::RightButton)]),
+            (Button::RightTrigger, paste_key.to_owned()),
+            (Button::LeftTrigger, copy_key.to_owned()),
+            (Button::West, vec![Arc::new(KeybdKey::EnterKey)]),
+            (Button::North, vec![Arc::new(KeybdKey::SpaceKey)]),
+            (Button::South, vec![Arc::new(KeybdKey::BackspaceKey)]),
+            (Button::East, super_key.to_owned()),
+        ];
+        let mut commands_map: ButtonsMap = HashMap::new();
+        for pair in _commands_map {
+            let (orig_button, mapped_seq) = pair;
+            commands_map.insert(orig_button, mapped_seq);
+        }
+        commands_map
+    };
+
+    static ref TypingMap: ButtonsMap = {
+        let typing_map = CommandsMap.clone();
+        typing_map
+    };
+}
 
 const MOUSE_SPEED: i32 = 5;
 const SCROLL_SPEED: i32 = 3;
@@ -22,32 +63,6 @@ fn main() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    let super_key = KeybdKey::OtherKey(0xffeb);
-    let copy_key: ButtonsVec = vec![box KeybdKey::LControlKey, box KeybdKey::CKey];
-    let paste_key: ButtonsVec = vec![box KeybdKey::LControlKey, box KeybdKey::VKey];
-
-    let _commands_map: Vec<ButtonsMapPair> = vec![
-        (Button::DPadDown, vec![box KeybdKey::DownKey]),
-        (Button::DPadUp, vec![box KeybdKey::UpKey]),
-        (Button::DPadLeft, vec![box KeybdKey::LeftKey]),
-        (Button::DPadRight, vec![box KeybdKey::RightKey]),
-        (Button::RightTrigger2, vec![box MouseButton::LeftButton]),
-        (Button::LeftTrigger2, vec![box MouseButton::RightButton]),
-        (Button::RightTrigger, paste_key),
-        (Button::LeftTrigger, copy_key),
-        (Button::West, vec![box KeybdKey::EnterKey]),
-        (Button::North, vec![box KeybdKey::SpaceKey]),
-        (Button::South, vec![box KeybdKey::BackspaceKey]),
-        (Button::East, vec![box super_key]),
-    ];
-    let mut commands_map: HashMap<Button, ButtonsVec> = HashMap::new();
-    for pair in _commands_map {
-        let (orig_button, mapped_seq) = pair;
-        commands_map.insert(orig_button, mapped_seq);
-    }
-
-    let typing_map = box commands_map.clone();
-
     let mut commands_mode = true;
 
     loop {
@@ -56,9 +71,9 @@ fn main() {
             println!("Event {:?} from device id {} at {:?}", event, id, time);
 
 
-            let mapping = match commands_mode {
-                true => &commands_map,
-                false => &typing_map,
+            let mapping: &ButtonsMap = match commands_mode {
+                true => &CommandsMap,
+                false => &TypingMap,
             };
 
             match event {
