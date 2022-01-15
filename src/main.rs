d6c55193366e33d129b30bf7eccc62e0333b6e24
Andrew <PositiveWay05@gmail.com>
2022-01-15 12:53:41 +0400

    working on local hasmap, buttons handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use gilrs::{Gilrs, Button, Event, EventType,Axis};
-use inputbot::{KeybdKey, MouseButton, MouseWheel, MouseCursor};
-use std::{thread::sleep, time::Duration};
-use std::collections::HashMap;
-
+#![feature(box_syntax)]
 #[macro_use]
 extern crate partial_application;
+
+use gilrs::{Gilrs, Button, Event, EventType::*, Axis};
+use inputbot::{KeybdKey, MouseButton, GenericButton, MouseWheel, MouseCursor};
+use std::{thread::sleep, time::Duration};
+use std::collections::{HashMap};
+
+type BoxedButton = Box<dyn GenericButton>;
+type ButtonsVec = Vec<BoxedButton>;
+type ButtonsMapPair = (Button, ButtonsVec);
+
+const MOUSE_SPEED: i32 = 5;
+const SCROLL_SPEED: i32 = 3;
 
 fn main() {
     let mut gilrs = Gilrs::new().unwrap();
@@ -14,33 +22,31 @@ fn main() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    let mouse_speed = 5;
-    let scroll_speed = 3;
+    let super_key = KeybdKey::OtherKey(0xffeb);
+    let copy_key: ButtonsVec = vec![box KeybdKey::LControlKey, box KeybdKey::CKey];
+    let paste_key: ButtonsVec = vec![box KeybdKey::LControlKey, box KeybdKey::VKey];
 
-    let super_key = (0xffeb);
-    let copy_key = (KeybdKey::LControlKey, KeybdKey::CKey);
-    let paste_key = (KeybdKey::LControlKey, KeybdKey::VKey);
-
-    let commands_map = HashMap::from([
-        (Button::DPadDown, (KeybdKey::DownKey)),
-        (Button::DPadUp, (KeybdKey::UpKey)),
-        (Button::DPadLeft, (KeybdKey::LeftKey)),
-        (Button::DPadRight, (KeybdKey::RightKey)),
-        (Button::RightTrigger2, (MouseButton::LeftButton)),
-        (Button::LeftTrigger2, (MouseButton::RightButton)),
+    let _commands_map: Vec<ButtonsMapPair> = vec![
+        (Button::DPadDown, vec![box KeybdKey::DownKey]),
+        (Button::DPadUp, vec![box KeybdKey::UpKey]),
+        (Button::DPadLeft, vec![box KeybdKey::LeftKey]),
+        (Button::DPadRight, vec![box KeybdKey::RightKey]),
+        (Button::RightTrigger2, vec![box MouseButton::LeftButton]),
+        (Button::LeftTrigger2, vec![box MouseButton::RightButton]),
         (Button::RightTrigger, paste_key),
         (Button::LeftTrigger, copy_key),
-        (Button::West, (KeybdKey::EnterKey)),
-        (Button::North, (KeybdKey::SpaceKey)),
-        (Button::South, (KeybdKey::BackspaceKey)),
-        (Button::East, super_key),
-        (Axis::LeftStickX, partial!(MouseCursor::move_rel => _, 0)),
-        (Axis::LeftStickY, partial!(MouseCursor::move_rel => 0, _)),
-        (Axis::RightStickY, (MouseWheel::scroll_ver)),
-        (Axis::RightStickX, (MouseWheel::scroll_hor)),
-    ]);
+        (Button::West, vec![box KeybdKey::EnterKey]),
+        (Button::North, vec![box KeybdKey::SpaceKey]),
+        (Button::South, vec![box KeybdKey::BackspaceKey]),
+        (Button::East, vec![box super_key]),
+    ];
+    let mut commands_map: HashMap<Button, ButtonsVec> = HashMap::new();
+    for pair in _commands_map {
+        let (orig_button, mapped_seq) = pair;
+        commands_map.insert(orig_button, mapped_seq);
+    }
 
-    let typing_map = commands_map.clone();
+    let typing_map = box commands_map.clone();
 
     let mut commands_mode = true;
 
@@ -49,22 +55,37 @@ fn main() {
         while let Some(Event { id, event, time }) = gilrs.next_event() {
             println!("Event {:?} from device id {} at {:?}", event, id, time);
 
+
             let mapping = match commands_mode {
-                true => commands_map,
-                false => typing_map,
+                true => &commands_map,
+                false => &typing_map,
             };
 
             match event {
-                EventType::ButtonPressed(button,code) => {
-                    mapping[button].press();
-                },
-                EventType::ButtonReleased(button, code) => {
-                    mapping[button].release();
-                },
-                EventType::AxisChanged(axis, value, code) => {
-                    let mouse_func = mapping[axis];
+                ButtonPressed(button, code) | ButtonReleased(button, code) => {
+                    if mapping.contains_key(&button) {
+                        let seq = &mapping[&button];
+                        match event {
+                            ButtonPressed(..) => {
+                                for key in seq {
+                                    key.press();
+                                }
+                            }
+                            ButtonReleased(..) => {
+                                for key in seq.into_iter().rev() {
+                                    key.release();
+                                }
+                            }
+                            _ => {}
+                        }
+                    } else {
+                        println!("Unmapped button");
+                        break;
+                    }
                 }
-                _ => println!("Error"),
+
+                AxisChanged(axis, value, code) => {}
+                _ => println!("Action handling is omitted"),
             }
         }
     }
