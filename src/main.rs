ed74fad39319197f5a72404a9ff091a75db8f63c
Andrew <PositiveWay05@gmail.com>
2022-01-18 00:40:02 +0400

    direct custom lib input

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,46 @@
-#![feature(box_syntax)]
-
 mod uinput_direct;
 
 #[macro_use]
 extern crate partial_application;
 
 use gilrs::{Gilrs, Button, Event, EventType::*, Axis};
-use inputbot::{KeybdKey, MouseButton, GenericButton, MouseWheel, MouseCursor};
 use std::{thread::sleep, time::Duration};
 use std::collections::{HashMap};
 use lazy_static::lazy_static;
-use std::sync::Arc;
 use cached::proc_macro::cached;
+use uinput::Device;
 
-type ArcButton = Arc<dyn GenericButton + Send + Sync + 'static>;
-type ButtonsVec = Vec<ArcButton>;
-type ButtonsMapPair = (Button, ButtonsVec);
+use uinput::event::ButtonsVec;
+use uinput::event::keyboard::Key;
+
 type ButtonsMap = HashMap<Button, ButtonsVec>;
 
 lazy_static! {
-    static ref super_key: ButtonsVec = vec![Arc::new(KeybdKey::OtherKey(0xffeb))];
-    static ref copy_key: ButtonsVec = vec![Arc::new(KeybdKey::LControlKey), Arc::new(KeybdKey::CKey)];
-    static ref paste_key: ButtonsVec = vec![Arc::new(KeybdKey::LControlKey), Arc::new(KeybdKey::VKey)];
+    static ref copy_key: ButtonsVec = vec![Key::LeftControl,Key::C];
+    static ref paste_key: ButtonsVec = vec![Key::LeftControl, Key::V];
 
-    static ref CommandsMap: ButtonsMap = {
-        let _commands_map: Vec<ButtonsMapPair> = vec![
-            (Button::DPadDown, vec![Arc::new(KeybdKey::DownKey)]),
-            (Button::DPadUp, vec![Arc::new(KeybdKey::UpKey)]),
-            (Button::DPadLeft, vec![Arc::new(KeybdKey::LeftKey)]),
-            (Button::DPadRight, vec![Arc::new(KeybdKey::RightKey)]),
-            (Button::RightTrigger2, vec![Arc::new(MouseButton::LeftButton)]),
-            (Button::LeftTrigger2, vec![Arc::new(MouseButton::RightButton)]),
-            (Button::RightTrigger, paste_key.to_owned()),
-            (Button::LeftTrigger, copy_key.to_owned()),
-            (Button::West, vec![Arc::new(KeybdKey::EnterKey)]),
-            (Button::North, vec![Arc::new(KeybdKey::SpaceKey)]),
-            (Button::South, vec![Arc::new(KeybdKey::BackspaceKey)]),
-            (Button::East, super_key.to_owned()),
-        ];
-        let mut commands_map: ButtonsMap = HashMap::new();
-        for pair in _commands_map {
-            let (orig_button, mapped_seq) = pair;
-            commands_map.insert(orig_button, mapped_seq);
-        }
-        commands_map
-    };
+    static ref CommandsMap: ButtonsMap = HashMap::from([
+        (Button::DPadDown, vec![Key::Down]),
+        (Button::DPadUp, vec![Key::Up]),
+        (Button::DPadLeft, vec![Key::Left]),
+        (Button::DPadRight, vec![Key::Right]),
+        (Button::RightTrigger2, vec![Key::LeftMouse]),
+        (Button::LeftTrigger2, vec![Key::RightMouse]),
+        (Button::RightTrigger, paste_key.to_vec()),
+        (Button::LeftTrigger, copy_key.to_vec()),
+        (Button::West, vec![Key::Enter]),
+        (Button::North, vec![Key::Space]),
+        (Button::South, vec![Key::BackSpace]),
+        (Button::East, vec![Key::LeftMeta]),
+    ]);
 
     static ref TypingMap: ButtonsMap = {
         let typing_map = CommandsMap.clone();
         typing_map
     };
+
+    static ref fake_device:Device = Device::init_mouse_keyboard();
+
 }
 
 const MOUSE_SPEED: i32 = 5;
@@ -70,7 +61,6 @@ fn main() {
         while let Some(Event { id, event, time }) = gilrs.next_event() {
             println!("Event {:?} from device id {} at {:?}", event, id, time);
 
-
             let mapping: &ButtonsMap = match commands_mode {
                 true => &CommandsMap,
                 false => &TypingMap,
@@ -82,14 +72,10 @@ fn main() {
                         let seq = &mapping[&button];
                         match event {
                             ButtonPressed(..) => {
-                                for key in seq {
-                                    key.press();
-                                }
+                                fake_device.press_sequence(seq);
                             }
                             ButtonReleased(..) => {
-                                for key in seq.into_iter().rev() {
-                                    key.release();
-                                }
+                                fake_device.release_sequence(seq);
                             }
                             _ => {}
                         }
