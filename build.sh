7497e2322dd9eba563f3ca73b3e947f4c5673749
Andrew <PositiveWay05@gmail.com>
2022-02-14 20:00:06 +0400

    remove copy from build

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 rustup update
 rustup default stable
-cd ~/CLionProjects/ControllerRust/
 rm -rf ./target
 cargo build --release
-rm ~/GolandProjects/ControllerGo/ControllerRust
-cp -f ./target/release/ControllerRust ~/GolandProjects/ControllerGo/
